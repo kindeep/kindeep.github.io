c27a31d1849c96389282b50a4c828cd5d246e654
Kindeep <kindeepsingh@gmail.com>
2018-12-02 11:44:33 -0500

    cosc.brocku.ca

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+window.onload = function() {
+	start();
+}
+
+function start() {
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	console.log(w);
+	console.log(h);
+}
+</script>
