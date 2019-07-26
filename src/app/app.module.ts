288dbdb32ab80709030e9c76fdf3f4e216973765
Kindeep <kk17xg@brocku.ca>
2019-07-25 22:36:44 -0400

    project component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProjectComponent } from './project/project.component';
+// angular material animation modules 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// material angular component modules 
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/material';
+// material card component 
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
