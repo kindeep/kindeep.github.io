926af665ff175aa54b5006103b7cce60ee6ddaf4
kargq <K@4g1L124>
2019-10-16 13:28:22 -0400

    more projcects added

--- src/app/projects-tab/projects-tab.component.ts
+++ src/app/projects-tab/projects-tab.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects-tab',
   templateUrl: './projects-tab.component.html',
   styleUrls: ['./projects-tab.component.scss']
 })
-export class ProjectsTabComponent implements OnInit {
+export class ProjectsTabComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    //  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    // Tweets
+    let ngJs: any;
+    const ngFjs = document.getElementsByTagName('script')[0];
+    const ngP = 'https';
+    // if (document.getElementById('twitter-wjs')) {
+      // document.removeChild(document.getElementById('twitter-wjs'));
+    // }
+    ngJs = document.createElement('script');
+    ngJs.id = 'twitter-wjs';
+    ngJs.src = ngP + '://platform.twitter.com/widgets.js';
+    ngFjs.parentNode.insertBefore(ngJs, ngFjs);
   }
 
 }
