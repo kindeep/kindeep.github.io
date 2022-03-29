9d99d1d2a6ad45a92ec737ce2ae58662cde67f30
kindeep <kindeepsingh@gmail.com>
2022-03-29 00:48:24 -0400

    Basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { Button } from "@mui/material";
 
-function App() {
+interface Props {
+  children: React.ReactElement;
+}
+
+function ElevationScroll(props: Props) {
+  const { children } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+function Navbar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: "flex" }}>
+      <Box sx={{ flexGrow: 0, p: 3 }}>
+        <Typography>
+          I'm <b>Kindeep Singh Kargil</b>, a Software Engineer in Toronto
+          specializing in Full-Stack Development.
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <Box
+            sx={{
+              bgcolor: "primary.main",
+              borderRadius: 6,
+              mx: 3,
+              p: 2,
+              flexGrow: 1,
+              display: "flex",
+            }}
+          >
+            <Typography noWrap>
+              <Button sx={{ mx: 1, color: "primary.contrastText" }}>
+                About Me
+              </Button>
+              <Button sx={{ mx: 1, color: "primary.contrastText" }}>
+                Projects
+              </Button>
+              <Button sx={{ mx: 1, color: "primary.contrastText" }}>
+                Games
+              </Button>
+            </Typography>
+          </Box>
+        </div>
+      </Box>
+    </Box>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
